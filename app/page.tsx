87d41373aa6d7627250d1f24189be7f245298f15
Yalexyo <Yalexyo@users.noreply.github.com>
2025-10-28 09:44:06 +0800

    Update to new Figma design node

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div style={{ width: "100vw", height: "100vh", margin: 0, padding: 0 }}>
       <iframe
-        src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fdesign%2FzlopxMxPXOa0m8SR1V67b4%2FOnce-UI-Official-Design-System--v3.4---Community-%3Fnode-id%3D10202-183497%26m%3Ddev"
+        src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fdesign%2FzlopxMxPXOa0m8SR1V67b4%2FOnce-UI-Official-Design-System--v3.4---Community-%3Fnode-id%3D27230-159953%26t%3DcutOaUTAyVp7cyqp-4"
         width="100%"
         height="100%"
         frameBorder="0"
